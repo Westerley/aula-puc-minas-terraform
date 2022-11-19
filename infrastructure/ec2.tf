b0a5a2cee2fa2e87b9411f92ec16148429407713
Westerley <westerreis_@hotmail.com>
2022-11-18 20:45:28 -0400

    alteração documentação

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -6,21 +6,19 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-*-20.04-amd64-server-*"]
   }
 }
-
 resource "aws_key_pair" "this" {
   key_name   = "${local.prefix}-key"
   public_key = var.aws_public_key
 }
-
 resource "aws_instance" "this" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.aws_instance_type
   count                       = 1
   key_name                    = aws_key_pair.this.key_name
   associate_public_ip_address = true
-
-  vpc_security_group_ids = [aws_security_group.sg-web.id]
-  subnet_id              = aws_subnet.this.id
+  user_data                   = file("../scripts/userdata.sh")
+  vpc_security_group_ids      = [aws_security_group.sg-web.id]
+  subnet_id                   = aws_subnet.this.id
   tags = merge(
     local.common_tags,
     {
--- infrastructure/security_group.tf
+++ infrastructure/security_group.tf
@@ -27,13 +27,6 @@ resource "aws_security_group" "sg-web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = merge(
     local.common_tags,
     {
